47df462b7ecb127c018be294ea851e4ebc15622b
David Silva <davidslv@users.noreply.github.com>
2025-03-17 18:00:42 +0000

    Test Vanilla::Support::TileType
    This should not be changed without breaking the specs.

--- lib/vanilla/support/tile_type.rb
+++ lib/vanilla/support/tile_type.rb
@@ -7,13 +7,38 @@ module Vanilla
         DOOR    = '/'.freeze,
         FLOOR   = '.'.freeze,
         PLAYER  = '@'.freeze,
+        MONSTER = 'M'.freeze,
         STAIRS  = '%'.freeze,
-        PASSAGE = '='.freeze,
         VERTICAL_WALL = '|'.freeze
       ].freeze
 
       def self.values
         VALUES
+      end
+
+      # Check if the provided tile is a valid tile type
+      # @param tile [String] The tile character to check
+      # @return [Boolean] true if the tile is valid, false otherwise
+      def self.valid?(tile)
+        VALUES.include?(tile)
+      end
+
+      # Check if the tile is walkable (can be traversed by player)
+      # @param tile [String] The tile character to check
+      # @return [Boolean] true if the tile is walkable, false otherwise
+      def self.walkable?(tile)
+        return false unless valid?(tile)
+
+        [EMPTY, FLOOR, DOOR, STAIRS].include?(tile)
+      end
+
+      # Check if the tile is a wall type (blocks movement)
+      # @param tile [String] The tile character to check
+      # @return [Boolean] true if the tile is a wall type, false otherwise
+      def self.wall?(tile)
+        return false unless valid?(tile)
+
+        [WALL, VERTICAL_WALL].include?(tile)
       end
     end
   end
---
+++ spec/lib/vanilla/support/tile_type_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+
+RSpec.describe Vanilla::Support::TileType do
+  describe '.values' do
+    it 'returns all the defined tile values' do
+      expect(described_class.values).to eq([
+        ' ',  # EMPTY
+        '#',  # WALL
+        '/',  # DOOR
+        '.',  # FLOOR
+        '@',  # PLAYER
+        'M',  # MONSTER
+        '%',  # STAIRS
+        '|'   # VERTICAL_WALL
+      ])
+    end
+  end
+
+  describe '.valid?' do
+    context 'with valid tiles' do
+      it 'returns true for all defined tile types' do
+        described_class.values.each do |tile|
+          expect(described_class.valid?(tile)).to be true
+        end
+      end
+    end
+
+    context 'with invalid tiles' do
+      it 'returns false for undefined tile types' do
+        invalid_tiles = ['x', 'A', '1', '?', '$', '*']
+        
+        invalid_tiles.each do |tile|
+          expect(described_class.valid?(tile)).to be false
+        end
+      end
+    end
+  end
+  
+  describe '.walkable?' do
+    it 'returns true for walkable tiles' do
+      walkable_tiles = [
+        described_class::EMPTY,
+        described_class::FLOOR,
+        described_class::DOOR,
+        described_class::STAIRS,
+      ]
+      
+      walkable_tiles.each do |tile|
+        expect(described_class.walkable?(tile)).to be true
+      end
+    end
+    
+    it 'returns false for non-walkable tiles' do
+      non_walkable_tiles = [
+        described_class::WALL,
+        described_class::VERTICAL_WALL,
+        described_class::PLAYER
+      ]
+      
+      non_walkable_tiles.each do |tile|
+        expect(described_class.walkable?(tile)).to be false
+      end
+    end
+    
+    it 'returns false for invalid tiles' do
+      expect(described_class.walkable?('X')).to be false
+    end
+  end
+  
+  describe '.wall?' do
+    it 'returns true for wall tiles' do
+      wall_tiles = [
+        described_class::WALL,
+        described_class::VERTICAL_WALL
+      ]
+      
+      wall_tiles.each do |tile|
+        expect(described_class.wall?(tile)).to be(true)
+      end
+    end
+    
+    it 'returns false for non-wall tiles' do
+      non_wall_tiles = [
+        described_class::EMPTY,
+        described_class::FLOOR,
+        described_class::DOOR,
+        described_class::STAIRS,
+        described_class::PLAYER
+      ]
+      
+      non_wall_tiles.each do |tile|
+        expect(described_class.wall?(tile)).to be(false)
+      end
+    end
+    
+    it 'returns false for invalid tiles' do
+      expect(described_class.wall?('X')).to be false
+    end
+  end
+end 
