ac25f4890f5f91ca478d4442f2262d4908e49027
David Silva <davidslv@users.noreply.github.com>
2025-03-18 16:43:39 +0000

    Implement core event system with file-based storage

---
+++ lib/vanilla/events.rb
@@ -0,0 +1,9 @@
+# Event system for Vanilla game
+# This file requires all components of the event system
+
+require_relative 'events/event'
+require_relative 'events/types'
+require_relative 'events/event_subscriber'
+require_relative 'events/storage/event_store'
+require_relative 'events/storage/file_event_store'
+require_relative 'events/event_manager'
