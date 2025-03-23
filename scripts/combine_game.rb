7ca17ee00ff28132ec6b4d1e08cbb638040a8267
David Silva <davidslv@users.noreply.github.com>
2025-03-23 20:14:36 +0000

    add script to combine all files into a single file

---
+++ scripts/combine_game.rb
@@ -0,0 +1,24 @@
+# Combines all files in a lib directory into a single file
+
+output_file = "combined_game.rb"
+directory = "/Users/davidslv/projects/vanilla/lib"  # Change this to your directory path if needed
+bin_play_location = "/Users/davidslv/projects/vanilla/bin/play.rb"
+
+File.open(output_file, "w") do |output|
+  files = Dir.glob("#{directory}/**/*.rb")
+  files.append(bin_play_location)
+
+  puts "Files found: #{files.size}"
+
+  files.each do |filename|
+    next if filename == "./#{File.basename(__FILE__)}"  # Skip this merger script
+    next if filename == "./#{output_file}"              # Skip the output file
+
+    output.puts "# Begin #{filename}"
+    output.puts File.read(filename)
+    output.puts "# End #{filename}"
+    output.puts "\n"
+  end
+end
+
+puts "Files merged into #{output_file}"
