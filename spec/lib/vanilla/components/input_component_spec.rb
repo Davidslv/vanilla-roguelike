6612b43b82547d298577e03ef397d4cf530feeb4
David Silva <davidslv@users.noreply.github.com>
2025-03-24 20:01:08 +0000

    ecs: InputComponent

--- spec/lib/vanilla/components/input_component_spec.rb
+++ spec/lib/vanilla/components/input_component_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe Vanilla::Components::InputComponent do
     it 'initializes with default values' do
       component = Vanilla::Components::InputComponent.new
       expect(component.move_direction).to be_nil
-      expect(component.action_triggered).to be false
-      expect(component.action_params).to eq({})
     end
   end
 
@@ -34,99 +32,29 @@ RSpec.describe Vanilla::Components::InputComponent do
     end
   end
 
-  describe '#set_action_triggered' do
-    let(:component) { Vanilla::Components::InputComponent.new }
-
-    it 'sets the action triggered flag' do
-      component.set_action_triggered(true)
-      expect(component.action_triggered).to be true
-    end
-
-    it 'sets action params when triggered' do
-      params = { target: 'door', action: 'open' }
-      component.set_action_triggered(true, params)
-      expect(component.action_triggered).to be true
-      expect(component.action_params).to eq(params)
-    end
-
-    it 'does not set action params when not triggered' do
-      params = { target: 'door', action: 'open' }
-      component.set_action_triggered(false, params)
-      expect(component.action_triggered).to be false
-      expect(component.action_params).to eq({})
-    end
-  end
-
-  describe '#action_params' do
-    let(:component) { Vanilla::Components::InputComponent.new }
-
-    it 'returns empty hash by default' do
-      expect(component.action_params).to eq({})
-    end
-
-    it 'returns the set action params' do
-      params = { target: 'door', action: 'open' }
-      component.set_action_triggered(true, params)
-      expect(component.action_params).to eq(params)
-    end
-  end
-
-  describe '#clear' do
-    let(:component) { Vanilla::Components::InputComponent.new }
-
-    it 'resets all input state' do
-      component.set_move_direction(:north)
-      component.set_action_triggered(true, { target: 'door' })
-
-      component.clear
-
-      expect(component.move_direction).to be_nil
-      expect(component.action_triggered).to be false
-      expect(component.action_params).to eq({})
-    end
-  end
-
-  describe '#data' do
+  describe '#to_hash' do
     let(:component) { Vanilla::Components::InputComponent.new }
 
     it 'returns a hash with the component data' do
       component.set_move_direction(:east)
-      component.set_action_triggered(true, { target: 'door' })
 
-      expect(component.data).to eq({
-                                     move_direction: :east,
-                                     action_triggered: true,
-                                     action_params: { target: 'door' }
-                                   })
+      expect(component.to_hash).to eq(
+        {
+          move_direction: :east
+        }
+      )
     end
   end
 
   describe '.from_hash' do
     it 'creates a component from a hash' do
       hash = {
-        move_direction: :west,
-        action_triggered: true,
-        action_params: { target: 'chest', action: 'open' }
+        move_direction: :west
       }
 
       component = Vanilla::Components::InputComponent.from_hash(hash)
 
       expect(component.move_direction).to eq(:west)
-      expect(component.action_triggered).to be true
-      expect(component.action_params).to eq({ target: 'chest', action: 'open' })
-    end
-
-    it 'handles missing action_params' do
-      hash = {
-        move_direction: :west,
-        action_triggered: true
-      }
-
-      component = Vanilla::Components::InputComponent.from_hash(hash)
-
-      expect(component.move_direction).to eq(:west)
-      expect(component.action_triggered).to be true
-      expect(component.action_params).to eq({})
     end
   end
 
